acc8857d84c18ae987a2fa06d02f4cd4ddc2ce79
zverevm <mzverev@ikerlan.es>
2020-03-03 15:04:02 +0100

    Change rquic/subprotocol.go formatting

--- rquic/subprotocol.go
+++ rquic/subprotocol.go
@@ -2,7 +2,7 @@ package rquic
 
 ////////////////////////////////////////////////////////////////////////// Scheme
 const (
-	SchemeNoCode uint8 = iota 	// 0x00
+	SchemeNoCode uint8 = iota // 0x00
 	SchemeXor
 	SchemeRlcSys
 	//SchemeRlc
@@ -18,42 +18,42 @@ const (
 ////////////////////////////////////////////////////////////////////////// Type
 // of rQUIC packet
 const (
-    TypeCoded uint8 = iota
-    TypeProtected
-    TypeUnprotected
-    TypeUnknown
+	TypeCoded uint8 = iota
+	TypeProtected
+	TypeUnprotected
+	TypeUnknown
 )
 
 ////////////////////////////////////////////////////////////////////////// Field
 // [   1st byte   ]
 // [                 Destination Connection ID                    ]
 // [ type  scheme ][    pkt id    ][  gen.  size  ][ seed / coeff ]
-const (
-    FieldSizeTypeScheme int = 1                                         // FieldSize
-    FieldSizeId         int = 1
-    FieldSizeGenSize    int = 1
-    // FieldSizeSeed    // This will depend on the scheme
-    
-    SrcHeaderSize int = FieldSizeTypeScheme + FieldSizeId
-    CodPreHeaderSize int = FieldSizeTypeScheme + FieldSizeId + FieldSizeGenSize
+const ( //---------------------------------------------------------------- FieldSize
+	FieldSizeTypeScheme int = 1
+	FieldSizeId         int = 1
+	FieldSizeGenSize    int = 1
+	// FieldSizeSeed    // This will depend on the scheme
+
+	SrcHeaderSize    int = FieldSizeTypeScheme + FieldSizeId
+	CodPreHeaderSize int = FieldSizeTypeScheme + FieldSizeId + FieldSizeGenSize
 )
-const (                                                                 // FieldPos
-    FieldPosTypeScheme  int = 0
-    FieldPosId          int = FieldPosTypeScheme + FieldSizeTypeScheme
-    FieldPosGenSize     int = FieldPosId         + FieldSizeId
-    FieldPosSeed        int = FieldPosGenSize    + FieldSizeGenSize
+const ( //---------------------------------------------------------------- FieldPos
+	FieldPosTypeScheme int = 0
+	FieldPosId         int = FieldPosTypeScheme + FieldSizeTypeScheme
+	FieldPosGenSize    int = FieldPosId + FieldSizeId
+	FieldPosSeed       int = FieldPosGenSize + FieldSizeGenSize
 )
 
 ////////////////////////////////////////////////////////////////////////// Mask
 const (
-    MaskType    uint8 = 0x80
-    MaskScheme  uint8 = 0x7F
+	MaskType   uint8 = 0x80
+	MaskScheme uint8 = 0x7F
 )
 
 ////////////////////////////////////////////////////////////////////////// Min & Max
 const (
-    MaxGf       int = 255 // GF(2**8)
-    MaxGenSize  uint8 = 64    // The bigger, the smaller goodput in RLNC
-    MinRatio    float64 = 8 // (g+r)(1-a) >= g; a = 11% --> g/r <= 8.0909
-    RxRedunMarg float64 = 2 // If more COD than this --> Pollution!
+	MaxGf       int     = 255 // GF(2**8)
+	MaxGenSize  uint8   = 64  // The bigger, the smaller goodput in RLNC
+	MinRatio    float64 = 8   // (g+r)(1-a) >= g; a = 11% --> g/r <= 8.0909
+	RxRedunMarg float64 = 2   // If more COD than this --> Pollution!
 )
